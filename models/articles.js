c8c8c7a3dfb4aed376044c8d33924c7ba64080a1
Jul1enF <j.furic@gmail.com>
2025-02-04 13:02:20 +0100

    corrections modèle articles

--- models/articles.js
+++ models/articles.js
@@ -2,17 +2,20 @@ const mongoose = require('mongoose')
 
 const articleSchema = mongoose.Schema({
     title : String,
-    sub_title : String,
+    sub_category : String,
     img_link : String,
-    img_public_id : String,
     img_margin_top : Number,
     img_margin_left : Number,
     img_zoom : Number,
+    img_ratio : Number,
     video_id : String,
     category : String,
-    text : String,
+    text1 : String,
+    text2 : String,
     createdAt : Date,
     author : String,
+    tags : Array,
+    media_link : String,
 })
 
 const Article = mongoose.model('articles', articleSchema)
